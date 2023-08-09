8ec7c062b2e85ef7e94a6b0f42f6515ccbe520c0
Dunaeff Eugenii <56464554+Gargooie@users.noreply.github.com>
2023-08-09 19:40:26 +0400

    add more art

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 document.getElementById('learnMoreBtn').addEventListener('click', function() {
     alert('Вы узнали больше о нашем продукте!');
+    
+    // Add animation class to main content
+    document.querySelector('.main-content').classList.add('animated');
 });
